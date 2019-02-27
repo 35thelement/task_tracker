369a05df2e667e069a1f3730944179b00e5eac9f
Ben Romine <elementnumber35@gmail.com>
2019-02-26 21:44:23 -0500

    Minutes must be greater than 1 and a multiple of 15

--- lib/task_tracker/times/time.ex
+++ lib/task_tracker/times/time.ex
@@ -16,5 +16,13 @@ defmodule TaskTracker.Times.Time do
     time
     |> cast(attrs, [:minutes, :user_id, :task_id])
     |> validate_required([:minutes, :user_id, :task_id])
+    |> validate_number(:minutes, greater_than: -1)
+    |> validate_change(:minutes, fn :minutes, minutes ->
+      if rem(minutes, 15) > 0 do
+        [minutes: "must be a multiple of 15"]
+      else
+        []
+      end
+    end)
   end
 end
